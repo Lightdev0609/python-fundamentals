b926cc687444bd9ed34b762b8e6b004374c078c9
Lightdev0609 <nourislam0609@gmail.com>
2025-07-02 17:00:27 +0100

    halfway done

---
+++ control_flow.py
@@ -0,0 +1,6 @@
+# What if you did this?
+lop=[1,2,3,4,5,6,7,8,9,10]
+for n in lop:
+    print(n)            # This would always print first
+    if n==5:
+        print('half way there champ')
---
+++ data_structures.py
@@ -0,0 +1,9 @@
+#a list
+num=['one','two','three']
+num.append('four')
+for n in num:
+    print(n)
+#dic
+dic={'uno':'one','dos':'two','tres':'three'}
+dic['nihil']='NIHIL'
+print(dic['nihil'])
